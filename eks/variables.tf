f55cffda1dfce9bf9e446c75f5c3f810aac8e4a0
sho naito <sho20100@gmail.com>
2019-05-20 15:10:09 +0900

    Add variables.tf

---
+++ eks/variables.tf
@@ -0,0 +1,12 @@
+variable "cluster_names" {
+  # 'shared' is constants
+  description = "(Optional)Cluster names using this VPC e.g.  { kubernetes.io/cluster/${cluster_name} = shared } ref: https://docs.aws.amazon.com/eks/latest/userguide/network_reqs.html",
+
+  type    = "map"
+  default = {}
+}
+
+variable "vpc_name" {
+  description = "(Required) Name of the VPC."
+}
+
--- eks/vpc.tf
+++ eks/vpc.tf
@@ -1,11 +1,15 @@
-variable "class_a_address" {
-  # NOTE ref: https://docs.aws.amazon.com/ja_jp/AmazonVPC/latest/UserGuide/VPC_Subnets.html
-  # VPC を作成する場合は、 RFC 1918 に指定されているように、プライベート IPv4 アドレス範囲から CIDR ブロック (/16 以下) を指定することをお勧めします。
-  # 10.0.0.0 - 10.255.255.255 (10/8 プレフィックス)
-  # 172.16.0.0 - 172.31.255.255 (172.16/12 プレフィックス)
-  # 192.168.0.0 - 192.168.255.255 (192.168/16 プレフィックス)
-  default = "10"
-}
+####### IMPORTANT NOTE #######
+#
+# !!!EKSが利用できるCIDRはRFC1918の範囲内!!!
+#
+# ドキュメント
+# ref: https://aws.amazon.com/about-aws/whats-new/2018/10/amazon-eks-now-supports-additional-vpc-cidr-blocks/
+# ref: https://docs.aws.amazon.com/ja_jp/AmazonVPC/latest/UserGuide/VPC_Subnets.html
+#
+# VPC を作成する場合は、 RFC 1918 に指定されているように、プライベート IPv4 アドレス範囲から CIDR ブロック (/16 以下) を指定することをお勧めします。
+# 10.0.0.0 - 10.255.255.255 (10/8 プレフィックス)
+# 172.16.0.0 - 172.31.255.255 (172.16/12 プレフィックス)
+# 192.168.0.0 - 192.168.255.255 (192.168/16 プレフィックス)
 
 data "aws_availability_zones" "available" {}
 
@@ -13,28 +17,30 @@ data "aws_availability_zones" "available" {}
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "${terraform.workspace}-vpc"
-  cidr = "${var.class_a_address}.0.0.0/16"
+  name   = "${var.vpc_name}"
+  cidr   = "10.0.0.0/16"
 
-  # 2018/07/20現在 ap regionはEKS対応していないのでEKS使う場合はus-westにする
+  # 2018/11/01現在 ap regionはEKS対応していないのでEKS使う場合はus-east, us-westにする
   azs              = [ "${data.aws_availability_zones.available.names[0]}",
                        "${data.aws_availability_zones.available.names[1]}",
                        "${data.aws_availability_zones.available.names[2]}" ]
 
-  public_subnets   = ["${var.class_a_address}.0.0.0/24", "${var.class_a_address}.0.1.0/24", "${var.class_a_address}.0.2.0/24"]
-  private_subnets  = ["${var.class_a_address}.0.100.0/18"]
-  database_subnets = ["${var.class_a_address}.0.200.0/22", "${var.class_a_address}.0.210.0/22", "${var.class_a_address}.0.220.0/22"]
+  public_subnets   = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets  = ["10.0.96.0/20", "10.0.112.0/20", "10.0.128.0/20"]
+  database_subnets = ["10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  # Your VPC must have DNS hostname and DNS resolution support. Otherwise, your worker nodes cannot register with your cluster. For more information, see Using DNS with Your VPC in the Amazon VPC User Guide.
+  enable_nat_gateway   = true
+  enable_vpn_gateway   = true
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   # ref: https://docs.aws.amazon.com/eks/latest/userguide/network_reqs.html
-  # TODO: variable化してvpc.tfのnameと合わせてEKS使うときだけ書きたい
-  tags = {
-    Terraform   = "true"
-    Environment = "${terraform.workspace}",
-    "kubernetes.io/cluster/${terraform.workspace}-eks-cluster" = "shared",
-  }
+  tags = "${merge(
+    map("Terraform",   "true",
+        "Environment", "${var.vpc_name}"),
+    "${var.cluster_names}"
+  )}"
 }
 
 resource "aws_network_acl" "private_acl" {
@@ -42,24 +48,13 @@ resource "aws_network_acl" "private_acl" {
   subnet_ids = ["${module.vpc.private_subnets}"]
 
   # inbound
-  # TODO: 全開放でいいか確認する
-  # application port全開放
+
+  # allow VPC internal access
   ingress {
     rule_no    = 100
     action     = "allow"
     protocol   = "tcp"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 32768
-    to_port    = 65535
-  }
-
-  # TODO: 意図を記述する
-  # (多分)VPC internal accessに対してすべてのportを許可する
-  ingress {
-    rule_no    = 200
-    action     = "allow"
-    protocol   = "tcp"
-    cidr_block = "${var.class_a_address}.0.0.0/16"
+    cidr_block = "10.0.0.0/16"
     from_port  = 0
     to_port    = 65535
   }
@@ -74,7 +69,8 @@ resource "aws_network_acl" "private_acl" {
   }
 
   tags {
-    Name = "${terraform.workspace}-private-acl"
+    Name = "${var.vpc_name}-private-acl",
+    EKSComponent = "true"
   }
 }
 
@@ -83,23 +79,24 @@ resource "aws_network_acl" "public_acl" {
   subnet_ids = ["${module.vpc.public_subnets}"]
 
   # inbound
+  # allow internal access(k8s require)
   ingress {
     rule_no    = 100
     action     = "allow"
     protocol   = "tcp"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 30000
+    cidr_block = "10.0.0.0/16"
+    from_port  = 0
     to_port    = 65535
   }
 
-  # TODO: 意図を記述する
-  # (多分)VPC internal accessに対してすべてのportを許可する
+  # allow application port(if needed direct access to NodePort)
+  # NodePort range(default: 30000-32767)だけで(おそらく)十分なはずなので要検証
   ingress {
     rule_no    = 200
     action     = "allow"
     protocol   = "tcp"
-    cidr_block = "${var.class_a_address}.0.0.0/16"
-    from_port  = 0
+    cidr_block = "0.0.0.0/0"
+    from_port  = 20000
     to_port    = 65535
   }
 
@@ -113,28 +110,8 @@ resource "aws_network_acl" "public_acl" {
   }
 
   tags {
-    Name = "${terraform.workspace}-public-acl"
+    Name = "${var.vpc_name}-public-acl",
+    EKSComponent = "true"
   }
 }
-
-/* TODO:
-気持ち的には private_subnet からのみallow
-RDSへのアクセス制御がnetwork aclを用いて行うのが適切なのかよくわかってないのでコメントアウト中
-
-resource "aws_network_acl" "db_acl" {
-  # inbound
-  ingress {
-    rule_no = 100
-    action = "allow"
-    protocol = "tcp"
-    cidr_block = "0.0.0.0/16"
-    from_port = 32768
-    to_port = 65535
-  }
-
-  tags {
-    Name = "${terraform.workspace}-db-acl"
-  }
-}
-*/
 
